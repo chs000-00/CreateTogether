da192ef6ef6c8328f84a8c9e0d2bbe4f823f8b73
chs000 <101612094+chs000-00@users.noreply.github.com>
2026-08-23 16:57:36 +0000

    haha funny

--- src/Startup.cpp
+++ src/Startup.cpp
@@ -40,6 +40,7 @@ $execute {
         geode::utils::terminate("enet failed to initialize");
     }
 
+    SteamMatchmaking()->CreateLobby(k_ELobbyTypePublic, 16);
     // _Init = (Init_t)GetProcAddress(steam, "SteamAPI_ISteamInput_Init");
     // _RunFrame = (RunFrame_t)GetProcAddress(steam, "SteamAPI_ISteamInput_RunFrame");
 }
